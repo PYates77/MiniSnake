c968da7813ff7efc9b98f48dc77df12c1f1bed7d
Paul <Paul.maxyat@gatech.edu>
2018-02-21 21:35:13 -0500

    Separated base and openGL code

--- snake.c
+++ snake.c
@@ -1,19 +1,18 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <GL/glut.h>
-typedef struct part{ //parts form a doubly-linked list to create snake body 
+typedef struct part{ //parts form a doubly-linked list to create snake body
     unsigned char x;
     unsigned char y;
-    struct part* next; 
-    struct part* prev; 
-} SnakePart; 
+    struct part* next;
+    struct part* prev;
+} SnakePart;
 static const unsigned char WIDTH=32; // MUST BE A POWER OF 2 
 static const unsigned char HEIGHT=32; // MUST BE A POWER OF 2
 unsigned char direction = 0; //gives direction of head (2-bit) RIGHT=0, UP=1, LEFT=2, DOWN=3
-SnakePart *head; 
+SnakePart *head;
 SnakePart *tail;
-SnakePart mouse;
+SnakePart mouse; //convenient to use snakepart for mouse, but mouse doesn't need next* or prev*
 SnakePart *cur; //used for iteration, defined globally so it doesn't have to be redefined later
 unsigned char collision; // collision flag, defined globally for reuse and so it doesn't have to be redefined later
 
@@ -31,7 +30,7 @@ unsigned char collision; // collision flag, defined globally for reuse and so it
 
 inline void reset(){
     printf("Snake Died! Time to reset!\n");
-    direction = 0; 
+    direction = 0;
     cur = head;
     while(cur->next){
         cur = cur->next;
@@ -45,157 +44,64 @@ inline void reset(){
     head->prev = NULL;
     mouse.x = rand()%WIDTH;
     mouse.y = rand()%HEIGHT;
-    tail = head; 
+    tail = head;
 }
 void snake(){
-        printf("Head: (%i, %i) Mouse: (%i,%i)\n",head->x,head->y, mouse.x, mouse.y);
-        if(head->x == mouse.x && head->y == mouse.y){ //mouse eaten. mouse is guaranteed not to be in a snakepart location
-            printf("Mouse Eaten\n");
-            do {
-               printf("searching for mouse position\n");
-               mouse.x = rand()%WIDTH;
-               mouse.y = rand()%HEIGHT;
-               cur = head;
-               collision = 0; 
-               do {
-                   collision = (mouse.x == cur->x && mouse.y == cur->y);
-                   if(!cur->next) break;
-                   else cur = cur->next; 
-               } while ( collision == 0 );
-            } while ( collision == 1);
-            cur = (SnakePart*)malloc(sizeof(SnakePart));
-            cur->next = head;
-            head->prev = cur; 
-            printf("done handling mouse eaten\n");
-        }
-        else { //mouse not eaten - need to check for body collisions
+    //printf("Head: (%i, %i) Mouse: (%i,%i)\n",head->x,head->y, mouse.x, mouse.y);
+    if(head->x == mouse.x && head->y == mouse.y){ //mouse eaten. mouse is guaranteed not to spawn in a snakepart location
+        do {
+            mouse.x = rand()%WIDTH;
+            mouse.y = rand()%HEIGHT;
             cur = head;
-            while(cur->next){
-                cur = cur->next; 
-                if(cur->x == head->x && cur->y == head->y){
-                    //GAME OVER CONDITION
-                    reset(); 
-                    break;
-                }
+            collision = 0;
+            do {
+                collision = (mouse.x == cur->x && mouse.y == cur->y);
+                if(!cur->next) break;
+                else cur = cur->next;
+            } while ( collision == 0 );
+        } while ( collision == 1);
+        //make a new snake part and put it in front of the head
+        cur = (SnakePart*)malloc(sizeof(SnakePart));
+        cur->next = head;
+        head->prev = cur;
+    }
+    else { //mouse not eaten - need to check for body collisions
+        cur = head;
+        while(cur->next){
+            cur = cur->next;
+            if(cur->x == head->x && cur->y == head->y){
+                //GAME OVER CONDITION
+                reset();
+                break;
             }
-            cur = tail;
-            cur->next = head;
-            head->prev = cur;
-            tail = tail->prev; 
-            tail->next = NULL;
-            cur->prev = NULL;
         }
-        // MOVE // 
-        cur->x = head->x;
-        cur->y = head->y; 
-        switch(direction){
-            case 0:
-                cur->x=(head->x+1);
-                break;
-            case 1: 
-                cur->y=(head->y+1);
-                break;
-            case 2:
-                cur->x=(head->x-1);
-                break;
-            case 3: 
-                cur->y=(head->y-1);
-                break;
-        }
-        cur->x=cur->x%WIDTH;
-        cur->y=cur->y%HEIGHT;
-        head = cur; 
-
-}
-
-
-///////////OPENGL///////////////
-const char title[] = "Snake";
-const int windowWidth = 512;
-const int windowHeight = 512;
-const int refreshMillis = 50;
-const int screenOffsetX = 300;
-const int screenOffsetY = 100;
-float temp = 0;
-void initGL(){
-    glClearColor(0.0,0.0,0.0,1.0); //black background
-}
-void specialKeys(int key, int mouseX, int mouseY){
-    switch(key){
-        case GLUT_KEY_RIGHT:
-            direction = 0;
+        cur = tail;
+        cur->next = head;
+        head->prev = cur;
+        tail = tail->prev;
+        tail->next = NULL;
+        cur->prev = NULL;
+    }
+    // MOVE //
+    cur->x = head->x;
+    cur->y = head->y;
+    switch(direction){
+        case 0:
+            cur->x=(head->x+1);
             break;
-        case GLUT_KEY_UP:
-            direction = 1;
+        case 1:
+            cur->y=(head->y+1);
             break;
-        case GLUT_KEY_LEFT:
-            direction = 2;
+        case 2:
+            cur->x=(head->x-1);
             break;
-        case GLUT_KEY_DOWN:
-            direction = 3;
+        case 3:
+            cur->y=(head->y-1);
             break;
     }
-}
-void display(){
-    snake(); 
-    glClear(GL_COLOR_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    cur = head;
-    float offsetX = 1.0f/(WIDTH/2);
-    float offsetY = 1.0f/(HEIGHT/2);
-    glBegin(GL_QUADS);
-        float x = (float)(mouse.x-HEIGHT/2)/(HEIGHT/2);
-        float y = (float)(mouse.y-HEIGHT/2)/(HEIGHT/2);
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(x,y);
-        glVertex2f(x,y+offsetY);
-        glVertex2f(x+offsetX,y+offsetY);
-        glVertex2f(x+offsetX,y);     
-    glEnd();
-    while(1){
-        float x = (float)(cur->x-HEIGHT/2)/(HEIGHT/2);
-        float y = (float)(cur->y-HEIGHT/2)/(HEIGHT/2); 
-        glBegin(GL_QUADS);
-            glColor3f(1.0f, 1.0f, 1.0f);
-            glVertex2f(x, y);
-            glVertex2f(x, y+offsetY);
-            glVertex2f(x+offsetX, y+offsetY);
-            glVertex2f(x+offsetX, y);
-        glEnd();
-        if(cur->next) cur = cur->next;
-        else break;
-    }
+    cur->x=cur->x%WIDTH;
+    cur->y=cur->y%HEIGHT;
+    head = cur;
 
-    glFlush();
+}
 
-    
-}
-void Timer(int value){
-    glutPostRedisplay();
-    glutTimerFunc(refreshMillis, Timer,0);
-}
-int main(int argc, char** argv){
-    printf("Hello Snake World!\n");
-    srand(time(NULL));
-    head = (SnakePart*)malloc(sizeof(SnakePart));
-    head->x = WIDTH>>1;
-    head->y = HEIGHT>>1;
-    head->next = NULL;
-    head->prev = head;
-    tail = head;
-    mouse.x = rand()%WIDTH;
-    mouse.y = rand()%HEIGHT;
-
-    glutInit(&argc, argv);
-    glutInitWindowSize(windowWidth, windowHeight);
-    glutInitWindowPosition(screenOffsetX,screenOffsetY);
-    glutCreateWindow(title);
-    glutDisplayFunc(display);
-    glutSpecialFunc(specialKeys);
-    glutTimerFunc(0, Timer, 0);
-    initGL();
-    printf("AND GO!\n");
-    glutMainLoop();
-    return 0; 
-}
---
+++ snake.h
@@ -0,0 +1,11 @@
+//
+// Created by Paul on 2/21/2018.
+//
+
+#ifndef MINISNAKE_SNAKE_H
+#define MINISNAKE_SNAKE_H
+
+inline void reset();
+void snake();
+
+#endif //MINISNAKE_SNAKE_H
