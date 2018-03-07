3213ef6453c1e9d9d5a819c0d3825933cc13f7a8
Paul Yates <paul@debbie>
2018-03-07 13:13:01 -0500

    Fixing problem with undefined reference. Need to fix the inline functions.

--- snake.c
+++ snake.c
@@ -14,7 +14,7 @@
 // Check head location against all body parts //
 // IF: collision -reset game //
 
-extern inline void snake_init(){
+inline void snake_init(){
     head = (SnakePart *)malloc(sizeof(SnakePart));
     head->x = WIDTH>>1;
     head->y = HEIGHT>>1;
@@ -26,7 +26,7 @@ extern inline void snake_init(){
     score = 0;
     direction = 0;
 }
-extern inline void snake_reset(){ //delete all non-head body parts, reset variables
+inline void snake_reset(){ //delete all non-head body parts, reset variables
     printf("Snake Died! Score: %i\n",score);
     cur = head;
     SnakePart* temp;
